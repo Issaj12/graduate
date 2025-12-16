f959df7878a909a020f50a7e0b4e0a3f0b7247aa
Issa-Josiah <g.lectricals@gmail.com>
2025-12-16 14:05:14 +0300

    end of home page

---
+++ Graduation/home/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+
+# Create your views here.
+def dashboard(request):
+    return render(request, 'home/dashboard.html')
