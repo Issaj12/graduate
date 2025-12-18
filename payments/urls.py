ce206e338aa9cf08830bf3bc4cf56b8218154467
Issaj12 <githua.mwangi22@students.dkut.ac.ke>
2025-12-18 17:26:24 +0300

    gunicorn and procfile

--- payments/urls.py
+++ payments/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
      path('pay/', views.initiate_payment, name='initiate_payment'),
     path('callback/', views.daraja_callback, name='daraja_callback'),
+    path('status/<int:transaction_id>/', views.transaction_status, name='transaction_status')
+
 ]
